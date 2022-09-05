c5bc67104e49978b767abbc88a827f1a0c23abf0
someshshakya <somesh@appscrip.co>
2022-09-05 13:10:12 +0530

    refactor( reduced time complexity)
    main
    
    1. reduced time complexity and tested the working

--- Array/Bubble_Sort.cpp
+++ Array/Bubble_Sort.cpp
@@ -2,22 +2,24 @@
 
 #include<iostream>
 using namespace std;
-const int N = 5;
+const int Length = 5;
 main(){
-    int initialNumber, i, j,LargeArray[N],Temp;
+    int initialNumber, i, j,SizeArray[Length],Temp;
     cout << "Enter your numbers : \n";
-    for (i = 0; i < N;i++)
-        cin >> LargeArray[i];
+    for (i = 0; i < Length;i++)
+        cin >> SizeArray[i];
     // sorting
-    for (i = 0; i < N; i++)
-        for (j = 0; j < i + 1;j++)
-            if (LargeArray[i] < LargeArray[j]){
-                Temp = LargeArray[i];
-                LargeArray[i] = LargeArray[j];
-                LargeArray[j] = Temp;
+    for (i = Length; i > 0;i--){
+        for (j = 0; j < i;j++){
+            if(SizeArray[j]>SizeArray[j+1]){
+                Temp = SizeArray[j];
+                SizeArray[j] = SizeArray[j+1];
+                SizeArray[j+1] = Temp ;
             }
-    cout << "Following are the sorted items of  Array is :- \n";
-    for (i = 0; i < N;i++)
-        cout << LargeArray[i] << "\n";
+        }
+    }
+        cout << "Following are the sorted items of  Array is :- \n";
+    for (i = 0; i < Length;i++)
+        cout << SizeArray[i] << "\n";
 }
 
